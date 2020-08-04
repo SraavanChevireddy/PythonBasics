2ea2f745afb69dc1cf6e1513b74783f4aa00de6a
Sraavan Chevireddy <schevireddy@saventech.com>
2020-08-04 18:04:40 +0530

    Range Collection added

--- __pycache__/Range.py
+++ __pycache__/Range.py
@@ -4,4 +4,12 @@
 # Range (start,stop) -----------> This have start and stop range
 # Range (start,stop,step) -----------> This contains a start and stop value along with the step function.
 
+# By default python will have the range from zero.
 print(range(5))
+
+# You can declare the range with start and stop values. 
+print(range(5,10))
+
+# You can convert the range to list with listOf(:_)
+print(list(range(5,10)))
+
